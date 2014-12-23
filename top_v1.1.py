dfed00678cbaaca401ccc650f26caa24dd690393
martindafonte <martindafonte@gmail.com>
2014-12-23 20:25:48 +0000

    Se hacen cambios en el lector de top para poder leer de fedora

--- top_v1.1.py
+++ top_v1.1.py
@@ -34,26 +34,30 @@ while 1:
 	# Procces x User
 	lineas_procesos = lineas[7:-1]
 	diccionario = {}
-	p = re.compile('\d+\s(\w+)')
+	reg_user = re.compile('\d+\s(\w+)')
 	for linea_proc in lineas_procesos:
-		m = p.findall(linea_proc.strip())
-		user = m[0]
+		res = reg_user.findall(linea_proc.strip())
+		user = res[0]
 		if (user in diccionario):
 			diccionario[user] += 1
 		else:
 			diccionario[user] = 1
 	JSON += '"process_per_user": '+ json.dumps(diccionario)+ ",\n"
-	thirdLine = lineas[2].split(",") # CPU use
-	tokens = thirdLine[0].split()
-	JSON += '"cpu_use": ' + tokens[1][-3] + "," + "\n"
-	fourthLine = lineas[3].split(",") # Memory use
+	cpu = re.sub(r'\x1b[^m]*m','',lineas[2].strip())
+	cpu = re.sub(r'[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff]', '', cpu)
+	reg_cpu = re.compile('([0-9\.]){1,5}\sid')
+	res = reg_cpu.findall(cpu)
+	cpu_use = 100 - float(res[0])
+	JSON += '"cpu_use": ' +  str(cpu_use) + "," + "\n"
+	fourthLine = re.sub(r'\x1b[^m]*m','',lineas[3])
+	fourthLine = re.sub(r'[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff]', '', fourthLine)
+	fourthLine = fourthLine.split(",") # Memory use
 	tokens = fourthLine[0].split()
-	memTotal = float(tokens[2][:-1])
+	memTotal = float(tokens[2][:-1]) 
 	tokens = fourthLine[1].split()
-	memUsed = float(tokens[1][:-1])
-	#print tokens
+	memUsed = float(tokens[0])
 	strPorcMem = str(round((memUsed/memTotal)*100,1))
 	JSON += '"memory_used": ' + strPorcMem + "\n"
-	#print "JSON\n",JSON	
+	print "JSON\n",JSON	
 	time.sleep(5)	
 print "TERMINO DE LEER DEL TOP"
