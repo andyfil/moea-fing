d0d3caa252231bd89d2af2285789a99f6677d2e7
martindafonte <martindafonte@gmail.com>
2015-03-30 22:43:56 +0000

    Se organiza las carpetas

---
+++ deprecated/server_v1.py
@@ -0,0 +1,31 @@
+import SocketServer
+import sys
+import json
+import dataHandler
+from fileHandler import FileHandler
+from dbHandler import BDHandler
+
+TCP_IP = '' #direccion ip donde escucha se deja vacia para escuchar en todas
+TCP_PORT = 80 #puerto donde escucha
+BUFFER_SIZE = 1024  #tamano del buffer, ajustarlo al tamano maximo del paquete json
+datos = []
+data_handler = None
+
+class TCPHandler(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+		self.data = self.request.recv(BUFFER_SIZE).strip()
+		if(self.data == '' or self.data == None):
+                        return
+                else:
+                        print "{} wrote".format(self.client_address[0])
+                        j = json.loads(self.data, object_hook=_decode_dict)
+                        dataHandler.saveJson(j)
+		
+if __name__ == "__main__":
+	if(len(sys.argv) > 1 and sys.argv[1] == "bd"):
+		data_handler = BDHandler()
+	else:
+		data_handler = FileHandler()
+	server = SocketServer.TCPServer((TCP_IP, TCP_PORT), TCPHandler)
+	server.serve_forever()
