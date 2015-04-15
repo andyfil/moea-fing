70928510182080bcf572b34c8bcc0dcf6f747af9
dfilgueiras89 <dfilgueiras89@66c1ecb2-d233-c9cb-6d40-eaacdfb9143f>
2015-04-15 23:26:32 +0000

    Script de creacion del modelo de base de datos de la parte web

--- info/inicializacion_bd.sql
+++ info/inicializacion_bd.sql
@@ -13,3 +13,41 @@ CREATE TABLE `fing`.`datos` (
   `memory_use` DECIMAL(10,2) NULL,
   PRIMARY KEY (`id`))
 COMMENT = 'Tabla que almacena los datos recibidos de las pc';
+
+CREATE TABLE "maquinas_salon" (
+    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nombre" varchar(200) NOT NULL,
+    "lugar" varchar(200) NOT NULL,
+    "prioridad" integer NOT NULL
+)
+;
+CREATE TABLE "maquinas_pc" (
+    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "salon_id" integer NOT NULL REFERENCES "maquinas_salon" ("id"),
+    "nombre" varchar(200) NOT NULL,
+    "mac" varchar(200) NOT NULL,
+    "so" varchar(100) NOT NULL,
+    "ram" integer NOT NULL,
+    "cpu" decimal NOT NULL,
+    "arq" varchar(50) NOT NULL,
+    "cant_cores" integer NOT NULL,
+    "estado" varchar(50) NOT NULL
+)
+;
+CREATE TABLE "maquinas_registropc" (
+    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "pc_id" integer NOT NULL REFERENCES "maquinas_pc" ("id"),
+    "fecha_alta" datetime NOT NULL
+)
+;
+CREATE TABLE "maquinas_lecturatop" (
+    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "pc_id" integer NOT NULL REFERENCES "maquinas_pc" ("id"),
+    "tiempo_lectura" datetime NOT NULL,
+    "cant_usuarios" integer NOT NULL,
+    "mem_perc" integer NOT NULL,
+    "cpu_perc" integer NOT NULL
+)
+;
+
+COMMIT;
